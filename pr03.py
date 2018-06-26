47e4d4fe3f23ae8895cf047a4479a8150854b81c
Naemku <formanship@gmail.com>
2018-06-26 19:24:12 +0900

    Practice no.03 & practice no.04

---
+++ pr03.py
@@ -0,0 +1,11 @@
+#Practice 03
+a=float(input("Your first number: "))
+b=float(input("Your second number: "))
+print(a//b)
+print(a/b)
+
+#Practice 04
+n=int(input("Your maximum number: "))
+for i in range(0,n):
+    print(n**2)
+
